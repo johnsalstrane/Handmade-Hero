87a71cb02f6aa04b3ef04d60a6ba4d4dc558e2f0
johnsalstrane <79778754+johnsalstrane@users.noreply.github.com>
2021-12-11 12:34:05 -1000

    Main Window Callback
    Added Win32MainWindowCallback, fixed WinMain to take proper parameters.

--- win32_handmade.cpp
+++ win32_handmade.cpp
@@ -1,6 +1,48 @@
 #include <windows.h>
 
-int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PWSTR CommandLine, int ShowCommand)
+#define internal static
+#define local_persist static
+#define global_variable static
+
+internal LRESULT CALLBACK Win32MainWindowCallback(HWND   Window,
+                                                    UINT   Message,
+                                                    WPARAM WParam,
+                                                    LPARAM LParam)
 {
+    LRESULT Result = 0;
+
+    switch (Message)
+    {
+    case WM_SIZE:
+    {
+    } break;
+    case WM_DESTROY:
+    {
+    } break;
+    case WM_CLOSE:
+    {
+    } break;
+    case WM_ACTIVATEAPP:
+    {
+    } break;
+    case WM_PAINT:
+    {
+    } break;
+    default:
+    {
+    } break;
+    }
+
+    return(Result);
+}
+
+int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLine, int ShowCommand)
+{
+    WNDCLASSA WindowClass = {};
+    WindowClass.style = CS_CLASSDC | CS_OWNDC;
+    WindowClass.lpfnWndProc = Win32MainWindowCallback;
+    WindowClass.hInstance = Instance;
+    WindowClass.lpszClassName = "HandmadeHeroClass";
+
 	return(0);
 }
